e67e1532c7eaf7f684699d9318341be20e1c3a87
“HOGE（ShutaYamada）” <hoge@hoge.jpyamadashuta10083150@gmail.com>
2024-07-10 23:18:00 +0900

    add: Departure編集API実装

--- app/Http/Controllers/DepartureController.php
+++ app/Http/Controllers/DepartureController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Departure\DepartureStoreRequest;
+use App\Http\Requests\Departure\DepartureUpdateRequest;
+use App\Models\Departure;
 use App\UseCases\Departure\DepartureIndexAction;
 use App\UseCases\Departure\DepartureStoreAction;
+use App\UseCases\Departure\DepartureUpdateAction;
 use Illuminate\Http\Request;
 
 class DepartureController extends Controller
@@ -17,5 +20,10 @@ class DepartureController extends Controller
     public function store(DepartureStoreRequest $request, DepartureStoreAction $action)
     {
         return $action($request);
+    }
+
+    public function update(DepartureUpdateRequest $request, Departure $departure, DepartureUpdateAction $action)
+    {
+        return $action($request, $departure);
     }
 }
