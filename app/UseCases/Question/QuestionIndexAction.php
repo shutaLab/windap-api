b3b546d8ea4c533d08e59fb9bdb12d3ed071e18f
ing-ShutaYamada <shuta.yamada@in-g.jp>
2024-10-24 21:51:22 +0900

    noteとquestionでuser_idをパラメータで受け取れるように

--- app/UseCases/Question/QuestionIndexAction.php
+++ app/UseCases/Question/QuestionIndexAction.php
@@ -5,14 +5,20 @@ namespace App\UseCases\Question;
 use App\Http\Requests\Question\QuestionIndexRequest;
 use App\Http\Resources\QuestionResource;
 use App\Models\Question;
+use App\Models\WindNote;
 
 class QuestionIndexAction
 {
     public function __invoke(QuestionIndexRequest $request)
     {
-        $questions = Question::with('user.userProfile','answers.user.userProfile')
-            ->orderBy('created_at', 'desc')
-            ->get();
+        $userId = $request->query('user_id');
+        $query = Question::with(['user.userProfile','answers.user.userProfile']);
+
+        if ($userId) {
+            $query->where('user_id', $userId);
+        }
+
+        $questions = $query->orderBy('created_at', 'desc')->get();
 
         return response()->json(QuestionResource::collection($questions));
     }
